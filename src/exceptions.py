5b3d1c21c1c1521e94bb6650b68247c2e458ffeb
Pavel <bruchkovskiiy@mail.ru>
2026-07-27 23:23:53 +0300

    feat: add custom exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,27 @@
+from fastapi import status, HTTPException
+
+
+class PaymentGatewayException(Exception):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Внутренняя ошибка платежного шлюза"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self.detail, *args, **kwargs)
+
+
+class OperationAlreadyExistsError(PaymentGatewayException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Операция с таким operationId уже существует"
+
+
+class PaymentGatewayHTTPException(HTTPException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Внутренняя ошибка платежного шлюза"
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class OperationAlreadyExistsHTTPException(PaymentGatewayHTTPException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Операция с таким operationId уже существует"
