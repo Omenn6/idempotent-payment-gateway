a203d24c9753b03fa1d625b405ad833121008ed5
Pavel <bruchkovskiiy@mail.ru>
2026-08-06 22:32:42 +0300

    test: add integration tests for get health

---
+++ tests/integration_tests/test_health.py
@@ -0,0 +1,30 @@
+from _pytest.monkeypatch import MonkeyPatch
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+
+from src.exceptions import DatabaseUnavailableError
+from src.services.operations import OperationService
+
+
+@pytest.mark.anyio
+async def test_health_check_success(client: AsyncClient):
+    response = await client.get("/health")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.content == b""
+
+
+@pytest.mark.anyio
+async def test_health_check_database_down(
+    client: AsyncClient, monkeypatch: MonkeyPatch
+):
+    async def mock_check_health(*args, **kwargs):
+        raise DatabaseUnavailableError()
+
+    monkeypatch.setattr(OperationService, "check_health", mock_check_health)
+
+    response = await client.get("/health")
+
+    assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
+    assert response.json() == {"detail": "База данных временно недоступна"}
