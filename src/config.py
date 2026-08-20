02f8e6b7b871be84a18653ef194c78d5a48b7ce2
Pavel <bruchkovskiiy@mail.ru>
2026-08-20 21:34:48 +0300

    chore: add fallback environment defaults for standalone docker deployment

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"]
+    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"] = "DEV"
 
     DB_HOST: str
     DB_PORT: int
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
-    PROVIDER_URL: str
+    PROVIDER_URL: str = "http://provider-simulator:8081"
 
     @property
     def DB_URL(self):
